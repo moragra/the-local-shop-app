2f1ae38d5ca3ee0666339a05f7969979d4d77b42
Graciela MacBook <moragraa@gmail.com>
2024-06-04 15:48:19 -0600

    created footer jsx and scss base

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
-import './Footer.scss'
+import { Link } from "react-router-dom";
+import "./Footer.scss";
 
 export default function Footer() {
   return (
-    <footer>
-        
+    <footer className="footer">
+      <div className="footer__left">
+        <h3 className="footer__left-header">Get Involved</h3>
+        <div className="footer__text">
+          <div className="footer__text-l">
+            <h4 className="footer__t">CREATE AN ACCOUNT</h4>
+            <h4 className="footer__t">SUBMIT A SHOP</h4>
+          </div>
+          <div className="footer__text-r">
+          <h4 className="footer__t"> ABOUT</h4>
+          <h4 className="footer__t">CONTACT US</h4>
+          </div>
+        </div>
+      </div>
+      <div className="footer__right">
+        <Link className="footer__right-link" to={"/"}>
+          <h2 className="footer__name">The</h2>
+          <h2 className="footer__name">Local Shop</h2>
+        </Link>
+      </div>
     </footer>
-  )
+  );
 }
