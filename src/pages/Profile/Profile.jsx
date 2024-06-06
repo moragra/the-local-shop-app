05fdb44157b83a013940aa010ecd92ce27f51bb7
Graciela MacBook <moragraa@gmail.com>
2024-06-05 20:06:22 -0600

    added profile funcitonality to display business and users data

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,82 +3,99 @@ import ig from "../../assets/instagram.svg";
 import fb from "../../assets/facebook.png";
 import x from "../../assets/twitter.svg";
 import li from "../../assets/linkedin.png";
-import edit from '../../assets/edit.svg'
+import edit from "../../assets/edit.svg";
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function Profile({token}) {
-  const [user, setUser] = useState(null)
-  const [business, setBusiness] = useState(null)
+export default function Profile({ token }) {
+  const [user, setUser] = useState(null);
+  const [business, setBusiness] = useState(null);
 
-  useEffect(()=>{
-    if(token){
-      getProfile()
-      getBusiness()
+  useEffect(() => {
+    if (token) {
+      getData();
     } else {
-      setUser(null)
-      setBusiness(null)
+      setUser(null);
+      setBusiness(null);
     }
-  }, [token])
+  }, [token]);
 
-  const getProfile = async () =>{
-    const {data} = await axios.get(`${import.meta.env.VITE_LOCALHOST}profile`, {
-      headers:{
+  const getData = async () => {
+    const resp1 = await axios.get(`${import.meta.env.VITE_LOCALHOST}profile`, {
+      headers: {
         Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      mode: 'cors'
-    })
-    setUser(data)
-  }
+      mode: "cors",
+    });
+    const profile = resp1.data;
+    setUser(profile);
 
-  const getBusiness = async () =>{
-    const {data} = await axios.get(`${import.meta.env.VITE_LOCALHOST}business/${user.id}`)
-  }
+    const resp2 = await axios.get(
+      `${import.meta.env.VITE_LOCALHOST}business/${profile.id}`
+    );
+    const business = resp2.data;
+    setBusiness(business);
+  };
 
   return (
     <>
-      <main className="profile">
-        <div className="profile__container">
-          <div className="profile__top">
-            <div className="profile__left">
-              <div className="profile__img">
-                <img className="profile__img-i" src="" alt="" />
+      {user && business ? (
+        <main className="profile">
+          <div className="profile__container">
+            <div className="profile__top">
+              <div className="profile__left">
+                <div className="profile__img">
+                  <img className="profile__img-i" src="" alt="" />
+                </div>
+                <div className="profile__name">
+                  <h2 className="profile__name-t">{user.name.toUpperCase()}</h2>
+                </div>
               </div>
-              <div className="profile__name">
-                <h2 className="profile__name-t">Graciela Mora Mejias</h2>
-              </div>
-            </div>
-            <div className="profile__right">
+              <div className="profile__right">
                 <img className="profile__right-i" src={edit} alt="" />
+              </div>
+            </div>
+            <hr className="profile__hr" />
+            <div className="profile__mid">
+              <h2 className="profile__mid-t">{business.shop_name.toUpperCase()}</h2>
+              <hr className="profile__mid-hr" />
+              <Link to={business.website_url} className="profile__mid-link">
+                <h2 className="profile__mid-t">WEBSITE</h2>
+              </Link>
+            </div>
+            <hr className="profile__hr" />
+            <div className="profile__socials">
+              <div className="profile__socials-ic">
+                <Link to={business.ig_url}>
+                  <img className="profile__socials-i" src={ig} alt="" />
+                </Link>
+              </div>
+              <hr className="profile__socials-hr" />
+              <div className="profile__socials-ic">
+                <Link to={business.fb_url}>
+                  <img className="profile__socials-i" src={fb} alt="" />
+                </Link>
+              </div>
+              <hr className="profile__socials-hr" />
+              <div className="profile__socials-ic">
+                <Link to={business.x_url}>
+                  <img className="profile__socials-i" src={x} alt="" />
+                </Link>
+              </div>
+              <hr className="profile__socials-hr" />
+              <div className="profile__socials-ic">
+                <Link to={business.li_url}>
+                  <img className="profile__socials-i" src={li} alt="" />
+                </Link>
+              </div>
             </div>
           </div>
-          <hr className="profile__hr" />
-          <div className="profile__mid">
-            <h2 className="profile__mid-t">SHOP NAME</h2>
-            <hr className="profile__mid-hr" />
-            <h2 className="profile__mid-t">WEBSITE</h2>
-          </div>
-          <hr className="profile__hr" />
-          <div className="profile__socials">
-            <div className="profile__socials-ic">
-              <img className="profile__socials-i" src={ig} alt="" />
-            </div>
-            <hr className="profile__socials-hr" />
-            <div className="profile__socials-ic">
-              <img className="profile__socials-i" src={fb} alt="" />
-            </div>
-            <hr className="profile__socials-hr" />
-            <div className="profile__socials-ic">
-              <img className="profile__socials-i" src={x} alt="" />
-            </div>
-            <hr className="profile__socials-hr" />
-            <div className="profile__socials-ic">
-              <img className="profile__socials-i" src={li} alt="" />
-            </div>
-          </div>
-        </div>
-      </main>
+        </main>
+      ) : (
+        ""
+      )}
     </>
   );
 }
